bb66a3575a7ad59d87125e83b656cbfe69f908eb
lalaghoury <lala.ghoury@gmail.com>
2024-05-03 01:05:24 +0500

    DONE

--- app.js
+++ app.js
@@ -1,24 +1,7 @@
 const app = require("express")();
 require("dotenv").config();
-
-process.on("unhandledRejection", (reason, promise) => {
-  console.error("Unhandled Rejection at:", promise, "reason:", reason);
-});
-
-// Middlewares
-
 const useMiddlwares = require("./utils/useMiddlwares");
-
-////////////////////////////////////////////////////////////////////////////////////////////////
-
-// Routes
-
 const useRoutes = require("./utils/useRoutes");
-
-////////////////////////////////////////////////////////////////////////////////////////////////
-
-// Database Connection
-
 const connectDB = require("./utils/db");
 const PORT = process.env.PORT || 8000;
 
