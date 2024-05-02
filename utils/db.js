0d4a675ae83defc258a2413896aa0cf59d44cb4f
lalaghoury <lala.ghoury@gmail.com>
2024-05-03 00:43:52 +0500

    DONE

--- utils/db.js
+++ utils/db.js
@@ -1,3 +1,5 @@
+const mongoose = require("mongoose");
+
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URL);
@@ -8,3 +10,5 @@ const connectDB = async () => {
     throw error;
   }
 };
+
+module.exports = connectDB;
