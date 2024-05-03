36dcb98230ac6d48961c1d0999118c57491877b9
lalaghoury <127485169+lalaghoury@users.noreply.github.com>
2024-05-02 21:52:17 -0700

    Update cors.js

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const useCors = (app) => {
   app.use(
     cors({
-      origin: ["https://euphoria-frontend-dnfjmhfka-aasil-ghourys-projects.vercel.app", "http://localhost:5173", "http://localhost:3000"],
+      origin: ["https://euphoria-frontend-dnfjmhfka-aasil-ghourys-projects.vercel.app", "http://localhost:5173", "http://localhost:3000", "https://euphoria-frontend-theta.vercel.app"],
       credentials: true,
     })
   );
