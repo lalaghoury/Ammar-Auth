a2822668f407451a60601e3b6fe71b6f9f299df8
lalaghoury <lala.ghoury@gmail.com>
2024-05-13 19:40:30 +0500

    bismillah

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -5,10 +5,12 @@ const useCors = (app) => {
     cors({
       origin: [
         "https://euphoria-frontend-dnfjmhfka-aasil-ghourys-projects.vercel.app",
+        "http://localhost:4173",
         "http://localhost:5173",
         "http://localhost:3000",
         "https://euphoria-frontend-theta.vercel.app",
         "https://euphoria-eight.vercel.app",
+        "https://euphoria-commerce.netlify.app",
       ],
       credentials: true,
     })
