94b61240c91f14c0b69ca3a7a9cfd62409bb56a4
lalaghoury <lala.ghoury@gmail.com>
2024-05-25 15:24:55 +0500

    Update express-session.js

--- middlewares/express-session.js
+++ middlewares/express-session.js
@@ -8,7 +8,7 @@ const useExpressSession = (app, mongooseConnection) => {
       resave: false,
       saveUninitialized: false,
       cookie: {
-        sameSite: process.env.NODE_ENV === "production" ? "None" : "strict",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
         secure: process.env.NODE_ENV === "production",
       },
       store: MongoStore.create({ mongoUrl: process.env.MONGO_URL }),
