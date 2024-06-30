dd1e557f0bde6a730e1abb57c5b548f7c39093ef
lalaghoury <lala.ghoury@gmail.com>
2024-06-30 08:05:23 +0500

    bismillah

--- routes/checkout.js
+++ routes/checkout.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const braintree = require("braintree");
 require("dotenv").config();
-const Cart = require("../models/Cart"); 
+const Cart = require("../models/Cart");
 const Order = require("../models/Order");
 const { requireSignin } = require("../middlewares/authMiddleware");
 const PendingOrder = require("../models/PendingOrder");
@@ -193,123 +193,105 @@ router.post("/stripe", requireSignin, async (req, res) => {
   }
 });
 
-router.post(
-  "/stripe_webhooks",
-  express.raw({ type: "application/json" }),
-  async (req, res) => {
-    const sig = req.headers["stripe-signature"];
-    let event;
+router.post("/stripe_webhooks", async (req, res) => {
+  let event = req.body;
 
-    try {
-      event = stripe.webhooks.constructEvent(
-        request.body,
-        sig,
-        process.env.STRIPE_WEBHOOK_ENDPOINT_SECRET
-      );
-    } catch (err) {
-      console.log(`Webhook signature verification failed: ${err.message}`);
-      return res.status(400).send(`Webhook Error: ${err.message}`);
-    }
+  switch (event.type) {
+    case "customer.discount.created":
+      // console.log(JSON.stringify(event));
+      const pending = await PendingOrder.findOne({
+        customer: event.data.object.customer,
+      });
 
-    switch (event.type) {
-      case "customer.discount.created":
-        // console.log(JSON.stringify(event));
-        const pending = await PendingOrder.findOne({
-          customer: event.data.object.customer,
-        });
+      if (pending) {
+        pending.coupon = event.data.object.coupon;
+        pending.couponApplied = true;
+        await pending.save();
+      }
 
-        if (pending) {
-          pending.coupon = event.data.object.coupon;
-          pending.couponApplied = true;
-          await pending.save();
+      break;
+
+    case "charge.updated":
+      // console.log(JSON.stringify(event));
+      const OldOrder = await PendingOrder.findOne({
+        customer: event.data.object.customer,
+      });
+
+      if (OldOrder) {
+        if (event.data.object.status === "succeeded") {
+          const newOrderObj = {
+            products: OldOrder.products,
+            user: OldOrder.user,
+            payment: {
+              cardType: event.data.object.payment_method_details.card.brand,
+              brand: event.data.object.payment_method_details.type,
+              last4: event.data.object.payment_method_details.card.last4,
+              exp_month:
+                event.data.object.payment_method_details.card.exp_month,
+              exp_year: event.data.object.payment_method_details.card.exp_year,
+              country: event.data.object.payment_method_details.card.country,
+              imageUrl:
+                event.data.object.payment_method_details.card.brand == "visa"
+                  ? "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Visa_Inc._logo.svg/1200px-Visa_Inc._logo.svg.png"
+                  : event.data.object.payment_method_details.card.brand ==
+                    "mastercard"
+                  ? "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoPXuA0BFfgjEKfl2QnL4YN8CIUIDrL6EO0A&s"
+                  : event.data.object.payment_method_details.card.brand == "jcb"
+                  ? "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBjpvE0FBfXQKB2dX6kZmgj_QqIhAAb31VuA&s"
+                  : event.data.object.payment_method_details.card.brand ==
+                    "discover"
+                  ? "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0GK-orUHwcn2GCeV6Gx19x5kP7wtYPz16Pw&s"
+                  : event.data.object.payment_method_details.card.brand ==
+                    "amex"
+                  ? "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNwk3UTLDWMD1ztUBVvUcgqgLpD8NOVJPiig&s"
+                  : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQN1evlHq4awm4ad_JIY1u3ibleMP6QtLI4-g&s",
+            },
+            shipping_address: {
+              address_line_1: event.data.object.shipping.address.line1,
+              address_line_2: event.data.object.shipping.address.line2,
+              city: event.data.object.shipping.address.city,
+              state: event.data.object.shipping.address.state,
+              country: event.data.object.shipping.address.country,
+              pincode: event.data.object.shipping.address.postal_code,
+              phone: event.data.object.shipping.phone,
+            },
+            billing_address: {
+              address_line_1: event.data.object.billing_details.address.line1,
+              address_line_2: event.data.object.billing_details.address.line2,
+              city: event.data.object.billing_details.address.city,
+              state: event.data.object.billing_details.address.state,
+              country: event.data.object.billing_details.address.country,
+              pincode: event.data.object.billing_details.address.postal_code,
+              phone: event.data.object.billing_details.phone,
+              email: event.data.object.billing_details.email,
+              name: event.data.object.billing_details.name,
+            },
+            coupon: event.data.object.coupon,
+            amount: event.data.object.amount / 100,
+          };
+
+          await new Order(newOrderObj).save();
+
+          await PendingOrder.findByIdAndDelete(OldOrder._id);
+
+          await Cart.findOneAndUpdate(
+            { userId: OldOrder.user },
+            {
+              $set: {
+                items: [],
+                total: 0,
+              },
+            }
+          );
         }
+      }
 
-        break;
-
-      case "charge.updated":
-        // console.log(JSON.stringify(event));
-        const OldOrder = await PendingOrder.findOne({
-          customer: event.data.object.customer,
-        });
-
-        if (OldOrder) {
-          if (event.data.object.status === "succeeded") {
-            const newOrderObj = {
-              products: OldOrder.products,
-              user: OldOrder.user,
-              payment: {
-                cardType: event.data.object.payment_method_details.card.brand,
-                brand: event.data.object.payment_method_details.type,
-                last4: event.data.object.payment_method_details.card.last4,
-                exp_month:
-                  event.data.object.payment_method_details.card.exp_month,
-                exp_year:
-                  event.data.object.payment_method_details.card.exp_year,
-                country: event.data.object.payment_method_details.card.country,
-                imageUrl:
-                  event.data.object.payment_method_details.card.brand == "visa"
-                    ? "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Visa_Inc._logo.svg/1200px-Visa_Inc._logo.svg.png"
-                    : event.data.object.payment_method_details.card.brand ==
-                      "mastercard"
-                    ? "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoPXuA0BFfgjEKfl2QnL4YN8CIUIDrL6EO0A&s"
-                    : event.data.object.payment_method_details.card.brand ==
-                      "jcb"
-                    ? "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBjpvE0FBfXQKB2dX6kZmgj_QqIhAAb31VuA&s"
-                    : event.data.object.payment_method_details.card.brand ==
-                      "discover"
-                    ? "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0GK-orUHwcn2GCeV6Gx19x5kP7wtYPz16Pw&s"
-                    : event.data.object.payment_method_details.card.brand ==
-                      "amex"
-                    ? "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNwk3UTLDWMD1ztUBVvUcgqgLpD8NOVJPiig&s"
-                    : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQN1evlHq4awm4ad_JIY1u3ibleMP6QtLI4-g&s",
-              },
-              shipping_address: {
-                address_line_1: event.data.object.shipping.address.line1,
-                address_line_2: event.data.object.shipping.address.line2,
-                city: event.data.object.shipping.address.city,
-                state: event.data.object.shipping.address.state,
-                country: event.data.object.shipping.address.country,
-                pincode: event.data.object.shipping.address.postal_code,
-                phone: event.data.object.shipping.phone,
-              },
-              billing_address: {
-                address_line_1: event.data.object.billing_details.address.line1,
-                address_line_2: event.data.object.billing_details.address.line2,
-                city: event.data.object.billing_details.address.city,
-                state: event.data.object.billing_details.address.state,
-                country: event.data.object.billing_details.address.country,
-                pincode: event.data.object.billing_details.address.postal_code,
-                phone: event.data.object.billing_details.phone,
-                email: event.data.object.billing_details.email,
-                name: event.data.object.billing_details.name,
-              },
-              coupon: event.data.object.coupon,
-              amount: event.data.object.amount / 100,
-            };
-
-            await new Order(newOrderObj).save();
-
-            await PendingOrder.findByIdAndDelete(OldOrder._id);
-
-            await Cart.findOneAndUpdate(
-              { userId: OldOrder.user },
-              {
-                $set: {
-                  items: [],
-                  total: 0,
-                },
-              }
-            );
-          }
-        }
-
-        break;
-      default:
-        console.log(`Unhandled event type ${event.type}.`);
-    }
-
-    res.send();
+      break;
+    default:
+      console.log(`Unhandled event type ${event.type}.`);
   }
-);
+
+  res.send();
+});
 
 module.exports = router;
