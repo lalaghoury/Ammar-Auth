b7cfdb93433b4147af02cf075d2beea376bfcf2c
lalaghoury <lala.ghoury@gmail.com>
2024-07-01 21:50:36 +0500

    ads

--- routes/checkout.js
+++ routes/checkout.js
@@ -4,16 +4,8 @@ const Cart = require("../models/Cart");
 const Order = require("../models/Order");
 const { requireSignin } = require("../middlewares/authMiddleware");
 const PendingOrder = require("../models/PendingOrder");
-const Stripe = require("stripe");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY || "sk_test_51OH0AiFpp281IlLHIvwwHb8y0ECoQmfKIR7U9hxxrJuK45nrLPdw8MfXbfE1WS9ORfoKeVBpHotDMYNCXfGNq8WB00hfsZpiuC");
 const rawJsonParser = require("../middlewares/custom-raw-parser");
-
-const stripeApiKey = process.env.STRIPE_SECRET_KEY;
-
-if (typeof stripeApiKey !== "string" || !stripeApiKey) {
-  throw new Error("Invalid API key");
-}
-
-const stripe = Stripe(stripeApiKey);
 
 router.post("/stripe", requireSignin, async (req, res) => {
   const { products, amount } = req.body;
