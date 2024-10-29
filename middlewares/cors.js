c209d50baf64dec0a91fbdfb9ff52053ffce1be6
lalaghoury <lala.ghoury@gmail.com>
2024-10-29 08:47:00 +0500

    Update cors.js

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -6,10 +6,7 @@ const useCors = (app) => {
       origin: [
         "http://localhost:4173",
         "http://localhost:5173",
-        "http://localhost:3000",
-        "https://euphoria-frontend-theta.vercel.app",
-        "https://euphoria-eight.vercel.app",
-        "https://euphoria-commerce.netlify.app",
+        process.env.CLIENT_URL,
         "*"
       ],
       credentials: true,
